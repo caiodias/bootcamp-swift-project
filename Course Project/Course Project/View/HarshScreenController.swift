2d24dc2cfa7d5faebdcd1bdb35e1ae57dedfc0da
Harsh <harsh.hbs@gmail.com>
2017-12-20 15:48:19 -0500

    created parseJSON

--- Course Project/Course Project/View/HarshScreenController.swift
+++ Course Project/Course Project/View/HarshScreenController.swift
@@ -23,7 +23,44 @@ class HarshScreenController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func parseJSON() {
+        let url = URL(string: "https://api.myjson.com/bins/pbeuv")
+        
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            
+            guard error == nil else {
+                print("returning error")
+                return
+            }
+            
+            guard let content = data else {
+                print("not returning data")
+                return
+            }
+            
+            
+            guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+                print("Not containing JSON")
+                return
+            }
+            
+            if let array = json["skills"] as? [String] {
+                self.tableArray = array
+                self.tableArray[0] = "My Skills"
+                
+            }
+            
+            print(self.tableArray)
+            
+            DispatchQueue.main.async {
+                self.tbl.reloadData()
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
 
     /*
     // MARK: - Navigation
