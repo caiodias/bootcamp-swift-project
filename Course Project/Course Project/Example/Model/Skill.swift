48fb823734148fe13a18e81781d1d12f793de108
Harsh <harsh.hbs@gmail.com>
2017-12-20 16:47:39 -0500

    Skill class added

---
+++ Course Project/Course Project/Example/Model/Skill.swift
@@ -0,0 +1,24 @@
+//
+//  Skill.swift
+//  Course Project
+//
+//  Created by Harsh Shah on 2017-12-20.
+//  Copyright © 2017 ProDigi. All rights reserved.
+//
+
+import Foundation
+
+public class Skill {
+    public let type: String
+    
+    
+    init(type: String) {
+        self.type = type
+        
+    }
+    
+    public func returnType() -> String {
+        return "Harsh Knows\(self.type) "
+    }
+}
+
