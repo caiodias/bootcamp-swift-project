b4a00d509b1d2333eacc079408571fc1bfea039d
Marcel Harvan <marcel.harvan@gmail.com>
2017-12-20 14:51:07 -0500

    Blog Cell

--- Course Project/Course Project/Example/View/BlogViewController.swift
+++ Course Project/Course Project/Example/View/BlogViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class BlogViewController: UIViewController {
+class BlogViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+
+    @IBOutlet weak var TableView: UITableView!
+    
+    var blog: Blog!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.TableView.delegate = self
+        self.TableView.dataSource = self
+        
 
         // Do any additional setup after loading the view.
     }
@@ -21,15 +30,14 @@ class BlogViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+ 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return
+    }
 
 }
---
+++ Course Project/Course Project/Example/View/Cell/BlogCell.swift
@@ -0,0 +1,24 @@
+//
+//  BlogCell.swift
+//  Course Project
+//
+//  Created by Marcel Harvan on 2017-12-20.
+//  Copyright © 2017 ProDigi. All rights reserved.
+//
+
+import UIKit
+
+class BlogCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var link: UILabel!
+    
+
+    func configureCell (blog: Blog) {
+        title.text = "\(blog.title)"
+        date.text = "\(blog.dateReleased)"
+        link.text = "\(blog.link)"
+    }
+
+}
